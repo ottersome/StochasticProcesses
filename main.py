47c4e4e27d60b46242da7073405da681a8400305
Luis Garcia <luisgarcia26284@gmail.com>
2022-10-03 23:58:13 +0800

    Polished out some blunders.

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 # Should be Run by default so it will be a script from the get go
 import sys
 import matplotlib.pyplot as plt
-from sp_sims.simulators.embeddedmc import *
+from sp_sims.simulators.stochasticprocesses import *
 from sp_sims.statistics.statistics import *
 
 def compare_stationary_event(length,initial_state):
@@ -16,34 +16,42 @@ def compare_stationary_event(length,initial_state):
     race_hold_tape, race_state_tape = race_sp.generate_history(initial_state)
 
     # Poisson Ev
+    # TODO change the sampling interval when needed
     poisson_sp = PoissonFight(length,1,rates)
-    poi_hold_tape, poi_state_tape = race_sp.generate_history(initial_state)
+    poi_hold_tape, poi_state_tape = poisson_sp.generate_history(initial_state)
     
     # Compare them with event drive
     emb_x_axis, emb_hist = eventd_stationary_state(emb_state_tape, emb_hold_tape)
     race_x_axis, race_hist = eventd_stationary_state(race_state_tape, race_hold_tape)
     poi_x_axis, poi_hist = eventd_stationary_state(poi_state_tape, poi_hold_tape)
-    print("Histogram Count:",emb_hist)
     fig,subs = plt.subplots(1,3)
-
-    subs[0].bar(emb_x_axis, emb_hist)
-    subs[0].set_title('Embedded Markov Chian')
-    subs[1].bar(race_x_axis, race_hist)
-    subs[1].set_title('Exponential Races')
-    subs[2].bar(poi_x_axis, poi_hist)
-    subs[2].set_title('Poison Fight')
     
-    # Get Probabilities
     emb_probs = emb_hist/np.sum(emb_hist)
     race_probs = race_hist/np.sum(race_hist)
     poi_probs = poi_hist/np.sum(poi_hist)
 
-    # Expected Values
-    emb_exp = emb_probs@emb_x_axis
-    race_exp = race_probs@race_x_axis
-    poi_exp = poi_probs@poi_x_axis
+    max_density = np.max([emb_probs.max(),race_probs.max(),poi_probs.max()])
 
-    print("ExpectedValue for Embedded MC: {}. ExpectedValue for Race: {}".format(emb_exp,race_exp))
+    subs[0].bar(emb_x_axis, emb_probs)
+    #  subs[0].hist(x=emb_x_axis, bins=len(emb_x_axis), weights=emb_probs,density=True)
+    subs[0].set_title('Embedded Markov Chian')
+    subs[0].set_ylim(bottom=0,top=max_density)
+    #  subs[1].hist(x=race_x_axis, bins=len(race_x_axis), weights=race_probs,density=True)
+    subs[1].bar(race_x_axis, race_probs)
+    subs[1].set_title('Exponential Races')
+    subs[1].set_ylim(bottom=0,top=max_density)
+    #  subs[2].hist(x=poi_x_axis, bins=len(poi_x_axis), weights=poi_probs,density=True)
+    subs[2].bar(poi_x_axis, poi_probs)
+    subs[2].set_title('Poison Fight')
+    subs[2].set_ylim(bottom=0,top=max_density)
+    
+    #
+    #  # Expected Values
+    #  emb_exp = emb_probs@emb_x_axis
+    #  race_exp = race_probs@race_x_axis
+    #  poi_exp = poi_probs@poi_x_axis
+
+    #  print("ExpectedValue for Embedded MC: {}. ExpectedValue for Race: {}".format(emb_exp,race_exp))
 
     plt.show()
 
@@ -52,12 +60,11 @@ def compare_stationary_sample(length,sampling_rate,initial_state):
     rates = {"lambda": 0.1,"mu":0.15} #This should keep us within the corner
     
     # Embeded Markov Chain Simulation
-    np.random.seed(123)
+    np.random.seed(int(time.time()))
     embedded_sp = EmbeddedMarkC_BD(length,rates)
     emb_hold_tape, emb_state_tape = embedded_sp.generate_history(initial_state)
 
     # Create Tapes for Race
-    np.random.seed(123)
     race_sp = RaceOfExponentials(length,rates)
     race_hold_tape, race_state_tape = race_sp.generate_history(initial_state)
     
@@ -75,21 +82,20 @@ def compare_stationary_sample(length,sampling_rate,initial_state):
     subs[1].bar(race_x_axis, race_hist)
     subs[1].set_title('Races')
 
-
     plt.show()
 
 
 if __name__ == '__main__':
-    # Create the two competitive processes
-    print("Starting with numerical analysis")
 
     if len(sys.argv) != 4:
-        print("Please provide lenth for simulations")
+        print("Please provide appropriate parameters for simulations")
         exit(-1)
 
+    # This does for now. Ill add argparse later if needed 
     length = int(sys.argv[1])
     sampling_rate = float (sys.argv[2])
     initial_state = int (sys.argv[3])
+
     compare_stationary_event(length,initial_state=initial_state)
     #  compare_stationary_sample(length,sampling_rate,0)
 
--- sp_sims/simulators/stochasticprocesses.py
+++ sp_sims/simulators/stochasticprocesses.py
@@ -42,8 +42,7 @@ class ExponentialMinP(StochasticSimulator):
     def generate_history():
         # Always generate new tape
         tape = np.array([self.length,len(self.rates)])
-        for i,rate in enumerate(self.rates):
-            tape[:,i]= np.random.exponential(self.n,self.p,self.length)
+        for i,rate in enumerate(self.rates): tape[:,i]= np.random.exponential(self.n,self.p,self.length)
         final_tape = np.min(tapes,1)# Get the minimum exponential at each instance
         # This should be equal in distribution to the holdin gtimes
 
@@ -72,19 +71,19 @@ class EmbeddedMarkC_BD(SPManager):
         self.s_prob = self.s_rate/(self.a_rate+self.s_rate)
 
 
-    # We also need our state transitions
+    # @@
     def generate_history(self, initial_state):
         exp_sp = ExponentialSumSP(self.length,self.a_rate+self.s_rate)
         self.holding_times = exp_sp.generate_history()
         
-        # We can initialize this before hand because the probability 
+        # We can initialize this beforehand because the probability 
         # distribution at every point is the same
         birth_or_death = np.random.choice([-1,1],self.length-1,p=[self.s_prob, self.a_prob])
         states = [initial_state]
 
         # We go through all the n-1 transitions
         for i in range(self.length-1):
-            if states[-1]==0:
+            if states[-1]==0 and birth_or_death[i] == -1: 
                 # In case of being at state 0 
                 # we only have the probability of moving right
                 new_time = np.random.exponential(scale=(1/self.a_rate))
@@ -92,6 +91,7 @@ class EmbeddedMarkC_BD(SPManager):
                 birth_or_death[i] = 1
 
             states.append(states[-1] + birth_or_death[i])
+        
         # Manually fix last holding time
         if states[-1] == 0 : self.holding_times[-1] = np.random.exponential(scale=(1/self.a_rate))
 
@@ -102,32 +102,36 @@ class EmbeddedMarkC_BD(SPManager):
         pass
 
 class PoissonFight(SPManager):
-    def __init__(self,amnt_events,sampling_interval,rates):
+
+    def __init__(self,length,sampling_interval,rates):
         # Sampling inteval will modify our rates
 
         self.rate_arr = rates['lambda']*sampling_interval
         self.rate_ser = rates['mu']*sampling_interval
         self.sampl_int = sampling_interval
-        self.amnt_evets = amnt_events
+        self.amnt_events = length
 
 
+        # Was playing with some ideas here, Dont think I need them for now
+        # V V V V V V V V V V V V V V V V V V V V V V V V V V V V V V V V V 
         # We fix rates to fit this intevals of time
-        self.time_length = amnt_events*(1/(self.rate_arr+self.rate_ser))
-        self.holding_variation = (1/(self.rate_arr+self.rate_ser)**2)
-        self.time_length += amnt_events*(2*self.holding_variation)# Just to hold the variations as well
+        #  self.time_length = length*(1/(self.rate_arr+self.rate_ser))
+        #  self.holding_variation = (1/(self.rate_arr+self.rate_ser)**2)
+        #  self.time_length += length*(2*self.holding_variation)# Just to hold the variations as well
 
     def generate_history(self,initial_state):
         # We have two poisson processes fighting 
-        birth = np.random.poisson(self.rate_arr,size=(amnt_events))
-        death = np.random.poisson(self.rate_death,size=(amnt_events))
+        birth = np.random.poisson(self.rate_arr,size=(self.amnt_events-1))
+        death = np.random.poisson(self.rate_ser,size=(self.amnt_events-1))
 
         trend = birth-death
 
-        for i in range(amnt_events):
-            movement = birth-death
-            if(states[-1] == 0):
-                movement = birth
-            states.append(min(states[-1] + movement,0))
+        states = [initial_state]
+        for i in range(self.amnt_events-1):
+            movement = trend[i]
+            if(states[i] == 0):
+                movement = birth[i]
+            states.append(max(states[-1] + movement,0))
 
         holding_times = np.full_like(states, self.sampl_int)
 
@@ -136,6 +140,7 @@ class PoissonFight(SPManager):
         pass
 
 
+# @@
 class RaceOfExponentials(SPManager):
 
     def __init__(self, length,rates):
@@ -158,22 +163,21 @@ class RaceOfExponentials(SPManager):
 
         states = [initial_state]
 
-        # Generate the Path
+        # Generate the path
         for i in range(self.length-1):
             cur_state = states[-1]
             change = bd[i]
-            if cur_state == 0:# We only take birth 
+            if cur_state == 0 and change == -1:# We only take birth 
                 holding_times[i] = race[i,1]
                 change = 1
             states.append(cur_state + change)
+
+        # Make sure last state is representative
+        if states[-1] == 0: holding_times = race[-1,1]
+
 
         return holding_times,states
 
     def simulate_n_processes(self):
         pass
         
-class FightOfBinomials(SPManager):
-
-    # These are determined by # deaths and # of births controlled by two binomial distributions
-    def __init__(self,length):
-        pass
